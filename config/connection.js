8a665f1a472368b300f3044c78b3c2324751b54a
TacTac <110001003+KJKhalil@users.noreply.github.com>
2023-02-08 01:07:03 -0500

    Completed server.js for the ports, express, and app.use for routes and to listen to the port

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
-const {connect, connection} = require("mongoose");
+const {connect, connection} = require('mongoose');
 
 const connectionUrl =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/SocialNet";
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/SocialNet';
 
 connect(connectionUrl, {
   useNewUrlParser: true,
