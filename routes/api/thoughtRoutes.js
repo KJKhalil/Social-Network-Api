378c666c1ab2e76d6f9561b0d7068c8c4c777507
TacTac <110001003+KJKhalil@users.noreply.github.com>
2023-02-08 01:29:08 -0500

    finished thoughtRoutes.js

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+
+const {
+    getAllThoughts,
+    getAThought,
+    updateThought,
+    createThought,
+    deleteThought,
+    createReaction,
+    deleteReaction
+} = require('../../controller/thoughtController');
+
+router.route('/')
+.get(getAllThoughts)
+.post(createThought);
+
+router
+.route('/:thoughtId')
+.get(getAThought)
+.put(updateThought)
+.delete(deleteThought);
+
+router
+.route('/:thoughtId/reactions')
+.post(createReaction);
+
+router
+.route('/:thoughtId/reactions/:reactionId')
+.delete(deleteReaction);
+
+module.exports = router;
