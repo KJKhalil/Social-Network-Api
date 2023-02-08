db9fdbfad39953c22b68ef8f31300f435e629f02
TacTac <110001003+KJKhalil@users.noreply.github.com>
2023-02-08 00:51:39 -0500

    Completed the connection code in connection.js

---
+++ config/connection.js
@@ -0,0 +1,12 @@
+const {connect, connection} = require("mongoose");
+
+const connectionUrl =
+  process.env.MONGODB_URI || "mongodb://localhost:27017/SocialNet";
+
+connect(connectionUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+
+module.exports = connection;
